8af5b9578fc2dc56fb019190582ecaa10da1726a
Emiliano Rubinsky <rubinskyemiliano@gmail.com>
2024-02-18 11:22:21 -0300

    Agrego nueva version app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,21 @@ const PORT = 3000;
 const productManager = new ProductManager('products.json');
 
 // Ruta para obtener todos los productos
+app.get('/', async (req, res) => {
+    try {
+        // Obtener todos los productos
+        let products = await productManager.getAllProducts();
+
+        // Enviar los productos como respuesta
+        res.json(products);
+    } catch (error) {
+        // Manejar errores
+        console.error('Error al obtener los productos:', error);
+        res.status(500).send('Error en ruta GET [/] - Error interno del servidor. ' + error.message);
+    }
+});
+
+// Ruta para obtener todos los productos, admitiendo un <limit> en los parametros tipo querystring
 app.get('/products', async (req, res) => {
     try {
         // Obtener el límite de resultados de la query param (?limit=)
@@ -21,8 +36,8 @@ app.get('/products', async (req, res) => {
         res.json(products);
     } catch (error) {
         // Manejar errores
-        console.error('Error al obtener los productos:', error);
-        res.status(500).send('Error interno del servidor');
+        console.error('Error en ruta GET [/products] - Error al obtener los productos:', error);
+        res.status(500).send('Error en ruta GET [/products] - Error interno del servidor. ' + error.message);
     }
 });
 
@@ -34,15 +49,15 @@ app.get('/products/:pid', async (req, res) => {
         const product = await productManager.getProductById(productId);
         // Si el producto no se encuentra, devolver un error 404
         if (!product) {
-            res.status(404).send('Producto no encontrado');
+            res.status(404).send('Producto específico no encontrado - productId:' + productId);
             return;
         }
         // Enviar el producto como respuesta
         res.json(product);
     } catch (error) {
         // Manejar errores
-        console.error('Error al obtener el producto:', error);
-        res.status(500).send('Error interno del servidor');
+        console.error('Error en ruta GET [/products/:id] - Error al obtener el producto particular:', error);
+        res.status(500).send('Error en ruta GET [/products/:id] - Error interno del servidor. ' + error.message);
     }
 });
 
